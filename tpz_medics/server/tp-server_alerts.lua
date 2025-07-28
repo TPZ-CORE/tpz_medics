51d21d478f3f7d1bae4bdd4518a1dae467b7d94e
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-28 08:50:24 +0300

    Update tp-server_alerts.lua

--- tpz_medics/server/tp-server_alerts.lua
+++ tpz_medics/server/tp-server_alerts.lua
@@ -1,3 +1,15 @@
 local TPZ = exports.tpz_core:getCoreAPI()
 
 local AlertArchives = {}
+
+-----------------------------------------------------------
+--[[ Base Events  ]]--
+-----------------------------------------------------------
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+
+    AlertArchives = nil
+end)
