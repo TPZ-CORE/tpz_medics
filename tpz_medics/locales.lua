89593760bf9e7730ec3079ac4bf72862dadf0158
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-28 10:23:58 +0300

    Update locales.lua

--- tpz_medics/locales.lua
+++ tpz_medics/locales.lua
@@ -2,21 +2,23 @@ Locales = {}
 
 Locales = {
 
-    ['PROMPT_ACTION']              = "Press",
+    ['PROMPT_ACTION']               = "Press",
 
-    ['NOT_CLOSE_FOR_ACTION']       = "~e~You have to be closer to the doctor to perform this action.",
-    ['NOT_DEAD']                   = "~e~You are not unconscious for this kind of medical treatment.",
-    ['DEAD']                       = "~e~You are unconscious, you need a full medical treatment.",
-    ['NOT_ENOUGH_MONEY']           = "~e~You don't have enough money to perform this action.",
-    ['NO_PLAYER_NEARBY']           = "~e~There are no players nearby to perform this action.",
-    ['NO_PLAYER_DEAD']             = "~e~There are no unconscious players nearby.",
+    ['NOT_CLOSE_FOR_ACTION']        = "~e~You have to be closer to the doctor to perform this action.",
+    ['NOT_DEAD']                    = "~e~You are not unconscious for this kind of medical treatment.",
+    ['DEAD']                        = "~e~You are unconscious, you need a full medical treatment.",
+    ['NOT_ENOUGH_MONEY']            = "~e~You don't have enough money to perform this action.",
+    ['NO_PLAYER_NEARBY']            = "~e~There are no players nearby to perform this action.",
+    ['NO_PLAYER_DEAD']              = "~e~There are no unconscious players nearby.",
 
-    ['WOUNDS']                     = "Performing wounds treatment..",
-    ['FULL']                       = "Performing full medical treatment..",
-    ['APPLYING']                   = "Applying syringe..",
+    ['WOUNDS']                      = "Performing wounds treatment..",
+    ['FULL']                        = "Performing full medical treatment..",
+    ['APPLYING']                    = "Applying syringe..",
 
-    ['TARGET_ACTION_ON_COOLDOWN']  = "The specified user has a cooldown of %s minute-s.",
-    ['ACTION_ON_COOLDOWN']         = "You can't perform this action for another %s minute-s.",
+    ['TARGET_ACTION_ON_COOLDOWN']   = "The specified user has a cooldown of %s minute-s.",
+    ['ACTION_ON_COOLDOWN']          = "You can't perform this action for another %s minute-s.",
 
-    ['NOT_REQUIRED_JOB']           = "~e~You don't have the required job.",
+    ['NOT_REQUIRED_JOB']            = "~e~You don't have the required job.",
+
+    ['ARCHIVE_ALERT_ALREADY_SIGNED'] = "The specified alert has been signed by someone else",
 }
