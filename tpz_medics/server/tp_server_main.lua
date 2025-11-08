39a0d00c19f8fdfb100e62d9e4b5c8da8e0d2eff
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-09 01:23:16 +0200

    Update tp_server_main.lua

--- tpz_medics/server/tp_server_main.lua
+++ tpz_medics/server/tp_server_main.lua
@@ -1,8 +1,74 @@
 local TPZ = exports.tpz_core:getCoreAPI()
 
+local Players = {}
+local PoisonedPlayers = {}
+
+-----------------------------------------------------------
+--[[ Functions ]]--
+-----------------------------------------------------------
+
+function GetPoisonedPlayers()
+  return PoisonedPlayers
+end
+
+-----------------------------------------------------------
+--[[ Base Events  ]]--
+-----------------------------------------------------------
+
+AddEventHandler('onResourceStop', function(resourceName)
+  if (GetCurrentResourceName() ~= resourceName) then
+    return
+  end
+
+  Players = nil
+  PoisonedPlayers = nil
+  
+end)
+
 -----------------------------------------------------------
 --[[ General Events ]]--
 -----------------------------------------------------------
+
+RegisterServerEvent("tpz_medics:server:request_data")
+AddEventHandler("tpz_medics:server:request_data", function()
+  local _source        = source
+  local xPlayer        = TPZ.GetPlayer(_source)
+  local charIdentifier = xPlayer.getCharacterIdentifier()
+
+  local isPoisoned = PoisonedPlayers[charIdentifier] and true or false
+
+  TriggerClientEvent("tpz_medics:client:update_requested_data", _source, { isPoisoned })
+end)
+
+RegisterServerEvent("tpz_medics:server:set_poisoned_state")
+AddEventHandler("tpz_medics:server:set_poisoned_state", function(targetSource, cb)
+  local _source = source
+  
+  if targetSource then 
+    _source = tonumber(targetSource)
+  end
+
+  local xPlayer        = TPZ.GetPlayer(_source)
+  local charIdentifier = xPlayer.getCharacterIdentifier()
+
+  local update = false
+
+  if PoisonedPlayers[charIdentifier] == nil and cb then 
+    PoisonedPlayers[charIdentifier] = 1
+    update = true
+  end
+
+  if not cb and PoisonedPlayers[charIdentifier] then 
+    PoisonedPlayers[charIdentifier] = nil 
+    update = true
+  end
+
+  if update then
+    TriggerClientEvent("tpz_medics:client:set_poisoned_state", _source, cb)
+  end
+
+end)
+
 
 RegisterServerEvent("tpz_medics:server:alert")
 AddEventHandler("tpz_medics:server:alert", function(unconscious)
@@ -94,3 +160,4 @@ AddEventHandler("tpz_medics:server:send_medical_entity_net", function(coords, ne
     coords = vector3(coords.x, coords.y, coords.z)
     TPZ.TriggerClientEventToCoordsOnly("tpz_medics:client:update_medical_entity_net", netId, coords, 150.0)
 end)
+
