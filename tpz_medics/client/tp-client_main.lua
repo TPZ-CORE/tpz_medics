4e4a73501bcc1cd5e28e6472038e0e66cd319ac9
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-14 01:39:41 +0200

    Update tp-client_main.lua

--- tpz_medics/client/tp-client_main.lua
+++ tpz_medics/client/tp-client_main.lua
@@ -17,6 +17,10 @@ function GetPlayerData()
     return PlayerData
 end
 
+exports('IsPlayerPoisoned', function()
+    return PlayerData.IsPoisoned
+end)
+
 -----------------------------------------------------------
 --[[ Base Events & Threads ]]--
 -----------------------------------------------------------
